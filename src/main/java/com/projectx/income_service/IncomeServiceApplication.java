9ed19368c28c3663efd741ab3ce32e1c3b55a44c
Angad-Raut <85058287+Angad-Raut@users.noreply.github.com>
2024-09-08 17:18:44 +0530

    changes commited

--- src/main/java/com/projectx/income_service/IncomeServiceApplication.java
+++ src/main/java/com/projectx/income_service/IncomeServiceApplication.java
@@ -2,14 +2,20 @@ package com.projectx.income_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class IncomeServiceApplication {
+public class IncomeServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IncomeServiceApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(IncomeServiceApplication.class);
+	}
 }
